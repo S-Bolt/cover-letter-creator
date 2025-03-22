7cee1e780c8025a9d44efded548c143f944f8ef9
Sam <sambolton.kw@gmail.com>
2025-03-21 21:11:20 -0400

    prisma fix

--- src/components/getStartedButton.js
+++ src/components/getStartedButton.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function GetStartedButton({ className = "" }) {
   return (
-    <Link href={"/cover-letter-generator"}>
+    <Link href={"/start"}>
       <button
         className={`text-sm bg-primary text-white  whitespace-nowrap  hover:bg-orange-700 active:shadow-inner transition sm:text-base sm:py-2 sm:px-6 button-3d  ${className}`}
       >
